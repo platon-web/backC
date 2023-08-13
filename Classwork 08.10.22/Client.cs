560d5393a4bb7a05f7168638ab269a660ff01310
Platon Holovetskyi <66998856+platon-web@users.noreply.github.com>
2023-08-13 22:02:11 +0300

    Add files via upload

---
+++ Classwork 08.10.22/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleApp17.Model;
+public class Client
+{
+    private readonly string name;
+    private readonly string someFieldOther = "some text";
+    private int client;
+    public string Method()
+    {
+        this.client = new();
+        return "Model.Client.Method had been called";
+    }
+    public void Method01(out int ints)
+    {
+        ints = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            ints += Random.Shared.Next(10, 100);
+        }
+    }
+    public int Sum(int a, int b) => a + b;
+    //{
+    //    return a+b;
+    //}
+}
+//private
+//protected
+//public
+
+//protected internal
+//internal - тип даних виден только в пределах сборки в которой он определен
---
+++ Classwork 08.10.22/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp17.Model;
+
+namespace ConsoleApp17
+   
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client =new Client();
+            client.Method01(out int result);
+            Console.WriteLine(result);
+        }
+    }
+}
