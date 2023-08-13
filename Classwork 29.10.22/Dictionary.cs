f0eb6b28d146635cd20b97719b5de847ff4cc38f
Platon Holovetskyi <66998856+platon-web@users.noreply.github.com>
2023-08-13 22:09:44 +0300

    Add files via upload

---
+++ Classwork 29.10.22/Dictionary.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+Dictionary<int, List<Person>> employees = new();
+
+employees.Add(34, new List<Person>());
+
+employees[34].Add(new Person() {​​​​​ FirstName = "Vasia" }​​​​​);
+
+employees[34].Add(new Person() {​​​​​ FirstName = "Petia" }​​​​​);
+
+employees[34].Add(new Person() {​​​​​ FirstName = "Olia" }​​​​​);
+
+employees[34].Add(new Person() {​​​​​ FirstName = "Vanya" }​​​​​);
+
+employees.Add(72, new List<Person>());
+
+employees[72].Add(new Person() {​​​​​ FirstName = "Roman" }​​​​​);
+
+employees[72].Add(new Person() {​​​​​ FirstName = "Victoria" }​​​​​);
+
+employees.Add(12, employees[72]);
+
+employees[72] = new List<Person>();
+
+employees[72].Add(new Person() {​​​​​ FirstName = "Sergei" }​​​​​);
+
+employees[72].Add(new Person() {​​​​​ FirstName = "Alexander" }​​​​​); foreach (var c in employees[34])
+
+    global::System.Console.WriteLine(c.FirstName);
+
+employees[34][1].Address = ""; 
+class Person
+{​​​​​
+
+    public string FirstName {​​​​​ get; set; }​​​​​
+
+    public string LastName {​​​​​ get; set; }​​​​​
+
+    public string Address {​​​​​ get; set; }​​​​​
+
+}​​​​​
+
---
+++ Classwork 29.10.22/Program.cs
@@ -0,0 +1,25 @@
+﻿Dictionary<long, Person> employees = new();
+employees.Add(6936438771, new Person() { FirstName = "Вася", LastName = "Пупкин", Address = "Some place on Earth" });
+employees.Add(7363141011, new Person() { FirstName = "Петя", LastName = "Васечкин", Address = "Some place on Earth" });
+
+employees.ContainsKey(7363141011);
+employees.ContainsValue(new Person() { FirstName = "Вася", LastName = "Пупкин", Address = "Some place on Earth" });
+
+employees[6936438771] = new Person() { FirstName = "Оля", LastName = ...};
+employees[6936438771].LastName = "NewLastName";
+
+Console.WriteLine(employees[6936438771]);
+
+
+foreach (var (key, value) in employees)
+{
+    Console.WriteLine($"Id: {key} FirsName: {value.FirstName} LastName: {value.LastName} Address: {value.Address}");
+}
+
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+}
