7af3db8f1fbbfc7fb75d78be88970646b5a31eb5
Platon Holovetskyi <66998856+platon-web@users.noreply.github.com>
2023-08-13 22:04:23 +0300

    Add files via upload

---
+++ Classwork 15.10.22/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    using System.Diagnostics.Metrics;
+    using System.Runtime.CompilerServices;
+
+    namespace FirstProject.Model
+    {
+        public class Client
+        {
+            private static int data;
+            static Client() { }
+            public Client() : this("Vasia") { }
+         
+
+            public Client(string name) : this(name, 18) { }
+            public Client(string name, int age)
+            {
+                Name = name;
+                Age = age;
+                Id = counter++;
+            }
+            public Client(Client src) : this(src.Name, src.Age) { }
+           
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int Id { get; }
+            private static int counter = 1;
+            public override string ToString()
+            {
+                return $"Name: {this.Name}\nAge: {this.Age}\nId: {this.Id}";
+            }
+            public static int GetSomeNumber()
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Classwork 15.10.22/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp22.FirstProject.Model;
+
+namespace FirstProject.Startup;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Client client1 = new();
+        Client client2 = new();
+        Client client3 = new();
+
+        Console.WriteLine(client1);
+        Console.WriteLine(client2);
+        Console.WriteLine(client3);
+    }
+}
