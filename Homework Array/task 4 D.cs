83eb85eb80cdfed77779fa72733ef9d07c691708
Platon Holovetskyi <66998856+platon-web@users.noreply.github.com>
2023-08-13 22:11:14 +0300

    Add files via upload

---
+++ Homework Array/task 4 D.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program3
+    {
+        static void Main(string[] args)
+        {
+            int N = 10, M = 20;
+            int[] arr01 = new int[N];
+            int[] arr02 = new int[M];
+            for (int i = 0; i < N; i++)
+            {
+                arr01[i] = Random.Shared.Next(10);
+                Console.Write(arr01[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < M; i++)
+            {
+                arr02[i] = Random.Shared.Next(10);
+                Console.Write(arr02[i] + " ");
+            }
+            Console.WriteLine();
+            int sizeResult = N < M ? N : M;
+            int[] arrResult = new int[sizeResult];
+            int resIter = 0;
+            for (int i = 0; i < sizeResult; i++)
+            {
+                bool leave = false;
+                for (int j = 0; j < sizeResult; j++)
+                {
+                    if (arr01[i] == arrResult[j])
+                        leave = true;
+                    if (leave)
+                        break;
+                }
+                if (leave)
+                    continue;
+                for (int j = 0; N < M ? j < N : j < M; j++)
+                    if (arr01[i] == arr02[j])
+                    {
+                        arrResult[resIter++] = arr01[i];
+                        break;
+                    }
+            }
+            foreach (int i in arrResult)
+                Console.Write(i + " ");
+        }
+    }
+}
---
+++ Homework Array/tast1.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.NetworkInformation;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chet = 0;
+            int neChet = 0;
+            int uniq = 0;
+            int  uniqi=20;
+            int[] arr = new int[20];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Random.Shared.Next(11);
+                Console.Write(" ");
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+            foreach(int item in arr)
+            {
+                if (item % 2 ==0 )
+                    chet++;
+                if(item % 2 !=0)
+                    neChet++;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j] && i != j)
+                    {
+                        uniq++;
+                        break;
+                    }
+                }
+
+            } 
+            uniqi -= uniq;
+            Console.WriteLine();
+            Console.Write("Chet ");
+            Console.Write(chet);
+            Console.WriteLine();
+            Console.Write("Ne Chet ");
+            Console.WriteLine(neChet);
+            Console.Write("uniq ");
+            Console.Write(uniqi);
+        }
+    }
+}
