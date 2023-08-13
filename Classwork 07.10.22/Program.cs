ad313007a384c34599befc2cac7dcd2f10ec57c8
Platon Holovetskyi <66998856+platon-web@users.noreply.github.com>
2023-08-13 22:01:29 +0300

    Add files via upload

---
+++ Classwork 07.10.22/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 101, max = 0;
+
+            int[,] arr = new int[3, 6];
+          
+            for (int i = 0; i < arr.GetLength(0); i++)
+            { 
+                for (int h = 0; h < arr.GetLength(1); h++)
+                {
+                   arr[i,h] =  Random.Shared.Next(10, 100)  ;
+                    Console.Write(arr[i,h]+" ");
+                    if (arr[i, h] > max)  
+                            max = arr[i,h]; 
+                   if(arr[i,h] < min)
+                        min = arr[i,h];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
---
+++ Classwork 07.10.22/task 2.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter words: ");
+            string enter = Console.ReadLine();
+            string[] text;
+            text = enter.Split(' ');
+            Console.Write("num words : ");
+            Console.WriteLine(text.Length);
+        }
+    }
+}
