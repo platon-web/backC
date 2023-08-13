b528c35bc892222bfdbec5bf5e4b6bf8f73fc49d
Platon Holovetskyi <66998856+platon-web@users.noreply.github.com>
2023-08-13 22:05:09 +0300

    Add files via upload

---
+++ classwork 22.10.22/Client.cs
@@ -0,0 +1,60 @@
+﻿namespace FirstProject.Model;
+
+
+public enum Material
+{
+    None = 0,
+    Steel = 1,
+    Aluminium = 2
+}
+
+public interface IMerch
+{
+    int Id { get;  }
+    string? Name { get; set; }
+    bool IsExpired { get; }
+    bool TakeOffTheShelf();
+}
+
+public class Screw : IMerch
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public Material Material { get; set; }
+    public double ScrewStep { get; set; }
+    public double Weight { get; set; }
+    public bool IsExpired { get; }
+
+    public bool TakeOffTheShelf()
+    {
+        return false;
+    }
+}
+
+public class Bulb : IMerch
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Power { get; set; }
+    public int LightTemperature { get; set; }
+    public int CapType { get; set; }
+    public bool IsExpired { get; }
+
+    public bool TakeOffTheShelf()
+    {
+        return true;
+    }
+}
+
+public class Glue : IMerch
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsExpired { get;  }
+
+    public bool TakeOffTheShelf()
+    {
+        return true;
+    }
+}
+
---
+++ classwork 22.10.22/Program.cs
@@ -0,0 +1,26 @@
+using FirstProject.Model;
+namespace FirstProject.CUI;
+
+class Program
+{
+    public static void Main()
+    {
+        Screw s01 = new();
+        Screw s02 = new();
+        Bulb bulb01 = new();
+        Bulb bulb02 = new();
+        Screw screw03 = new();      
+
+        List<IMerch> products = new();
+        products.Add(s01);
+        products.Add(s02);
+        products.Add(bulb01);
+        products.Add(bulb02);
+        products.Add(screw03);        
+        
+
+        foreach (var current in products)
+            if (current.IsExpired)
+                current.TakeOffTheShelf();
+    }
+}
